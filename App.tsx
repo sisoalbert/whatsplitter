5f47e8ae34c97f4d10a17f398b97cd9266d4d3fb
sisoalbert <sisoalbert@yahoo.com>
2024-05-01 01:48:07 +0200

    Added the camera

--- App.tsx
+++ App.tsx
@@ -1,35 +1,109 @@
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  Image,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
+import {
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
+} from 'react-native-vision-camera';
 
-const App = () => {
+const PermissionsPage = () => {
+  const {requestPermission} = useCameraPermission();
+
+  React.useEffect(() => {
+    requestPermission();
+  }, [requestPermission]);
   return (
     <SafeAreaView>
-      <Text>Whatsapp Video Edit</Text>
-      <View
-        style={{
-          width: 100,
-          height: 100,
-          borderRadius: 50,
-          backgroundColor: 'red',
-        }}></View>
-      <View
-        style={{
-          width: 100,
-          height: 100,
-          borderRadius: 50,
-          backgroundColor: 'yellow',
-        }}></View>
-      <View
-        style={{
-          width: 100,
-          height: 100,
-          borderRadius: 50,
-          backgroundColor: 'green',
-        }}></View>
+      <Text>Permissions is required.</Text>
     </SafeAreaView>
   );
 };
 
+const NoCameraDeviceError = () => {
+  return (
+    <View>
+      <Text>No camera device found.</Text>
+    </View>
+  );
+};
+
+function App() {
+  const device = useCameraDevice('back');
+  const {hasPermission} = useCameraPermission();
+  const cameraRef: any = React.useRef(null);
+  const [photo, setPhoto] = React.useState(null);
+
+  console.log(photo);
+
+  const takePhoto = async () => {
+    if (cameraRef.current) {
+      const options = {quality: 0.8, flashMode: 'on'};
+      const capturedPhoto = await cameraRef.current.takePhoto(options);
+      setPhoto(capturedPhoto.path);
+    }
+  };
+
+  if (!hasPermission) return <PermissionsPage />;
+  if (device == null) return <NoCameraDeviceError />;
+
+  return (
+    <>
+      {photo ? (
+        <>
+          <Image
+            style={{flex: 1}}
+            source={{
+              uri: `file://'${photo}`,
+            }}
+          />
+          <TouchableOpacity
+            onPress={() => setPhoto(null)}
+            style={{
+              borderColor: 'black',
+              borderWidth: 2,
+              justifyContent: 'center',
+              alignItems: 'center',
+              padding: 10,
+            }}>
+            <Text>Back</Text>
+          </TouchableOpacity>
+        </>
+      ) : (
+        <View style={StyleSheet.absoluteFill}>
+          <Camera
+            style={StyleSheet.absoluteFill}
+            device={device}
+            isActive={true}
+            photo={true}
+            ref={cameraRef}
+          />
+          <TouchableOpacity
+            onPress={takePhoto}
+            style={{
+              position: 'absolute',
+              bottom: 10,
+              alignSelf: 'center',
+              backgroundColor: 'white',
+              height: 50,
+              width: 50,
+              borderRadius: 25,
+              borderColor: 'black',
+              borderWidth: 2,
+              justifyContent: 'center',
+            }}
+          />
+        </View>
+      )}
+    </>
+  );
+}
 export default App;
 
 const styles = StyleSheet.create({});
